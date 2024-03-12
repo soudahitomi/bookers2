a0506cb589c9f72c2b1e9fc51ce4b4841682000c
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-03-12 07:40:29 +0000

    bookのindexのview作った

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/show'
-  get 'books/edit'
   devise_for :users
   root to: 'homes#top'
   get 'homes/about'
+  resources :books, only: [:index, :create, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
