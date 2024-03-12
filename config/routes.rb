198c8de95bebba287aabd8c5a86988ce824e9561
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-03-12 09:52:33 +0000

    必要だと思われるコントローラ、ビューの作成完

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about'
   resources :books, only: [:index, :create, :show, :edit]
+  resources :users, only: [:index, :edit, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
