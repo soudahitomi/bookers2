33b70f79dd467e638da1c233a2583867e7b7db8a
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-03-21 07:39:38 +0000

    フラッシュ完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,10 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    end
   end
 
   def show
@@ -28,7 +30,8 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
+    if book.update(book_params)
+        flash[:notice] = "you have updated book successfully."
     redirect_to book_path
   end
 
